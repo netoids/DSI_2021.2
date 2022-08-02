21ac366107c2bb08210b137a2df3f570ee8894bf
netoids <75789529+netoids@users.noreply.github.com>
2022-08-02 15:32:41 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Startup Name Generator',
       home: Scaffold(
         appBar: AppBar(
